01814599bff8f455eaf43459578c7a3872ed0fc7
TdewT <szwej.jakub@gmail.com>
2026-02-11 19:26:25 +0100

    Added aggregation to endpoints
    Added health endpoint for /api/v1

---
+++ src/fast_api/api/v1/endpoints/health.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends
+
+from fast_api.dependencies import get_node_service
+from fast_api.services.info import InfoService
+
+router = APIRouter(prefix="/api/v1")
+
+
+@router.get("/")
+def health_check(info_service: InfoService = Depends(get_node_service)):
+    """Health check endpoint for the shared API."""
+    return {"status": "ok", "version": info_service.get_app_version()}
--- src/fast_api/router.py
+++ src/fast_api/router.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
-from fast_api.dependencies import get_node_service
-from fast_api.services.info import InfoService
+from fast_api.api.v1.endpoints import health
 
-router = APIRouter(prefix="/api/v1", tags=["data"])
+router = APIRouter()
 
-
+# Top level API endpoints
 @router.get("/")
-def health_check(info_service: InfoService = Depends(get_node_service)):
+def health_check():
     """Health check endpoint for the shared API."""
-    return {"status": "ok", "version": info_service.get_app_version()}
+    return {"status": "ok"}
+
+# Lower level API endpoints
+router.include_router(health.router)
