350f72a65278515db58999e486358bea01e9d42d
TdewT <szwej.jakub@gmail.com>
2026-02-18 17:42:46 +0100

    Added exception for bulk reading inserts

--- src/fast_api/exceptions/database_exceptions.py
+++ src/fast_api/exceptions/database_exceptions.py
@@ -17,3 +17,26 @@ class ReadingInsertException(GenericDatabaseException):
         base = super().to_dict()
         base["reading-dump"] = self.reading.model_dump(mode="json")
         return base
+
+
+class ReadingsBulkInsertException(GenericDatabaseException):
+    def __init__(self, message: str,
+                 readings: list[SensorReading],
+                 exception: Exception | None = None,
+                 status_code: int = 500):
+
+        super().__init__(status_code=status_code, message=message)
+        self.readings = readings
+        self.exception = exception
+
+    def to_dict(self):
+        base = super().to_dict()
+        if self.exception:
+            if hasattr(self.exception, "to_dict") and callable(getattr(self.exception, "to_dict")):
+                base["exception"] = self.exception.to_dict()
+            else:
+                base["exception"] = str(self.exception)
+
+        reading_dumps = [reading.model_dump(mode="json") for reading in self.readings]
+        base["reading-dumps"] = reading_dumps
+        return base
